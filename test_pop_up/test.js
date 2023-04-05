70e513987db68ee53168c9b993734673a3f67e74
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-04-05 16:16:11 +0700

    Delete test pop up folder

--- test_pop_up/test.js
+++
@@ -1,9 +0,0 @@
-/* Open the pop-up form */
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-
-/* Close the pop-up form */
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
