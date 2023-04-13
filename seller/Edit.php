bf945123628696f2bde35955f4d72c1e13a8121d
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-04-13 19:38:53 +0700

    Edit product

--- seller/Edit.php
+++ seller/Edit.php
@@ -21,4 +21,3 @@ if(isset($_POST['Edit'])) {
     }
 }
 ?>
-
--- seller/delete.php
+++ seller/delete.php
@@ -1,8 +1,8 @@
 <?php
     $pro_id=$_GET['sid'];
-    require_once('connect.php');
+    include('../connect.php');
     $sql="DELETE FROM product WHERE product_id=$pro_id
     ";
     mysqli_query($conn, $sql);
-    header('Location: http://localhost/TamPHP/FinalPHP/Food_store_website/adminseller.php?');
+    header('Location: http://localhost/Food_store_website/seller/Adminseller.php');
 ?>
